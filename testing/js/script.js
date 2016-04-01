050ad962ad310a30bc6459820e9e6342cd469a9c
Jonathan Feudi <jonathanfeudi@gmail.com>
2016-04-01 13:34:48 -0400

    Drag and resize for img inside of frame implemented. Can enable/disable both dragging and resizing on both frame and frame contents.

--- testing/js/script.js
+++ testing/js/script.js
@@ -4,6 +4,8 @@ $(document).ready(function(){
 
 var frameCount = 0;
 var gridIncrement = 0;
+var frameContentDragLocked;
+var frameContentResizeLocked;
 
 function roundOff(position, grid){
   var remainder = position % grid;
@@ -20,9 +22,53 @@ function normalizeSnap(ind, el){
   el.style.left = roundOff(el.style.left.split('px')[0], gridIncrement)+'px';
 };
 
+$("#madsButton").click(function(){
+  $("#frame0").append("<div class=frameContent><img id=madsPic src=img/mads.jpg></div>");
+});
+
+$("#frameContentResize").click(function(){
+  if (frameContentResizeLocked){
+    frameContentResizeLocked = false;
+    $(".frameContent").children(["<img>"]).resizable("enable")
+  } else {
+    frameContentResizeLocked = false;
+    $(".frameContent").children(["<img>"]).resizable({
+      aspectRatio:true
+    })
+  }
+});
+
+$("#frameContentResizeLock").click(function(){
+  if (frameContentResizeLocked == false){
+    frameContentResizeLocked = true;
+    $(".frameContent").children(["<img>"]).resizable({
+      disabled:true
+    })
+  }
+});
+
+$("#frameContentDrag").click(function(){
+  if (frameContentDragLocked){
+    frameContentDragLocked = false;
+    $(".frameContent").draggable("enable");
+  } else {
+    frameContentDragLocked = false;
+    $(".frameContent").draggable();
+  }
+});
+
+$("#frameContentLock").click(function(){
+  if (frameContentDragLocked == false){
+    frameContentDragLocked = true;
+    $(".frameContent").draggable({
+      disabled:true
+    })
+  }
+});
+
 $("#frameButton").click(function(){
   if ($("#heightInput").val() && $("#widthInput").val()){
-    $("#container").append("<div id=frame"+frameCount+" style=height:"+$("#heightInput").val()+"px;width:"+$("#widthInput").val()+"px; class=frame>frame</div>");
+    $("#container").append("<div id=frame"+frameCount+" style=height:"+$("#heightInput").val()+"px;width:"+$("#widthInput").val()+"px; class=frame></div>");
     $("#frame"+frameCount).draggable();
     $("#frame"+frameCount).resizable();
     frameCount ++;
@@ -32,7 +78,7 @@ $("#frameButton").click(function(){
 });
 
 $("#testFrame").click(function(){
-  $("#container").append("<div id=frame"+frameCount+" style=height:100px;width:100px; class=frame>frame</div>");
+  $("#container").append("<div id=frame"+frameCount+" style=height:100px;width:100px; class=frame></div>");
   $("#frame"+frameCount).draggable();
   $("#frame"+frameCount).resizable();
   frameCount ++;
