99d4557f9170de5b89f97786cdded23d2457d424
Jonathan Feudi <jonathanfeudi@gmail.com>
2016-04-07 13:04:11 -0400

    Uhhhggg

--- testing/js/editor.js
+++
@@ -1,9 +0,0 @@
-window.onload = function(){
-  var editor;
-  ContentTools.StylePalette.add([
-    new ContentTools.Style('Author', 'author', ['p'])
-  ])
-};
-
-editor = ContentTools.EditorApp.get();
-editor.init('*[data-editable]', 'data-name');
